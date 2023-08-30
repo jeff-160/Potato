568ef1482a1c8f7c3375c0f3c23a94b5d7917439
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-08-30 23:34:23 +0800

    Add files via upload

--- Potato.hpp
+++ Potato.hpp
@@ -2,8 +2,17 @@
 #include "SDL2/SDL_image.h"
 #include "SDL2/SDL_ttf.h"
 
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <functional>
+
 #include "src/Engine.hpp"
 #include "src/Character.hpp"
+#include "src/Effects.hpp"
+#include "src/Choices.hpp"
 
 // fucky wucky main signature replace
 #define main() main(int argc, char* argv[])
