d7ed8a3c298461634b3f1c8ad2a1b67b779bdb6e
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-08-31 21:02:32 +0800

    Add files via upload

--- src/Character.hpp
+++ src/Character.hpp
@@ -4,6 +4,8 @@ namespace Potato{
             int CurrentFrame = 0;
 
         public:
+            friend class Engine;
+
             std::string Name;
             double X, Y;
             int Width, Height;
@@ -17,18 +19,10 @@ namespace Potato{
         
         Character(std::string Name, int X, int Y, int Width, int Height, std::vector<std::string> Images, int FrameRate=30): 
             Name(Name), X(X), Y(Y), Width(Width), Height(Height), Images(Images), FrameRate(FrameRate){
-
         }
     };
 
     void Character::Speak(std::string Text){
         
-    }
-
-    void Character::Draw(SDL_Renderer* Renderer){
-        SDL_Texture* CTexture = IMG_LoadTexture(Renderer, this->Images[this->CurrentFrame].c_str());
-        SDL_Rect CBounds = {this->X, this->Y, this->Width, this->Height};
-        SDL_RenderCopy(Renderer, CTexture, nullptr, &CBounds);
-        SDL_DestroyTexture(CTexture);
     }
 }
--- src/Engine.hpp
+++ src/Engine.hpp
@@ -1,45 +1,50 @@
 namespace Potato{
     class Engine{
         private:
+            SDL_Window* Window;
+            SDL_Renderer* Renderer;
+            
             std::string Name;
+            std::string Background;
             
             Uint32 FrameStart;
             int FrameTime, FPS = 100;
 
             Uint32 TextSpeed = 30;
 
-            void Close();
-            void MainLoop();
-
-        public: 
-            const int Width = 900;
-            const int Height = 600;
-
-            SDL_Window* Window;
-            SDL_Renderer* Renderer;
-            
             std::map<int, std::function<void()>> Story;
             std::vector<Character*> Scene;
+
+            void Error(std::string e);
+            void Close();
+            void MainLoop();
+            void Render(std::string is, int x, int y, int w, int h);
+
+        public: 
+            const int ScreenWidth = 900;
+            const int ScreenHeight = 600;
             
             void Run();
+            void SetStory(std::map<int, std::function<void()>> sm);
             void SetTextSpeed(Uint32 ts);
             void ClearScene();
-            void SceneAdd(Character* ca);
-            void SceneRemove(Character* ca);
+            void SceneAddCharacter(Character* ca);
+            void SceneRemoveCharacter(Character* ca);
+            void SceneSetBackground(std::string bs);
         
 
         Engine(std::string Name, std::string WindowIcon=""): Name(Name){
             this->Window = SDL_CreateWindow(
                                         this->Name.c_str(), 
                                         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
-                                        this->Width, this->Height, 
+                                        this->ScreenWidth, this->ScreenHeight, 
                                         SDL_WINDOW_SHOWN);
             this->Renderer = SDL_CreateRenderer(this->Window, -1, SDL_RENDERER_ACCELERATED);
 
             if (WindowIcon=="") return;
             SDL_Surface* Icon = IMG_Load(WindowIcon.c_str());
             if (Icon==nullptr){
-                std::cerr<<"Invalid icon file path: " << WindowIcon << std::endl;
+                this->Error("Invalid icon file path: " +  WindowIcon);
                 return;
             }
             SDL_SetWindowIcon(this->Window, Icon);
@@ -47,6 +52,10 @@ namespace Potato{
         }
     };
 
+    void Engine::Error(std::string err){
+        std::cerr<<"Error: " << err << std::endl;
+        return;
+    }
 
     void Engine::Run(){
         ShowWindow(GetConsoleWindow(), SW_HIDE);
@@ -73,8 +82,9 @@ namespace Potato{
         SDL_SetRenderDrawColor(this->Renderer, 0,0,0,255);
         SDL_RenderClear(this->Renderer);
 
+        this->Render(this->Background, 0, 0, this->ScreenWidth, this->ScreenHeight);
         for(auto c:this->Scene)
-            c->Draw(this->Renderer);
+            this->Render(c->Images[c->CurrentFrame], c->X, c->Y, c->Width, c->Height);
 
         SDL_RenderPresent(this->Renderer);
 
@@ -83,6 +93,16 @@ namespace Potato{
             SDL_Delay(this->FPS-this->FrameTime);
     }
 
+    void Engine::Render(std::string ImgSrc, int X, int Y, int Width, int Height){
+        SDL_Texture* CTexture = IMG_LoadTexture(this->Renderer, ImgSrc.c_str());
+        SDL_Rect CBounds = {X, Y, Width, Height};
+        SDL_RenderCopy(this->Renderer, CTexture, nullptr, &CBounds);
+        SDL_DestroyTexture(CTexture);
+    }
+
+    void Engine::SetStory(std::map<int, std::function<void()>> SMap){
+        this->Story = SMap;
+    }
 
     void Engine::SetTextSpeed(Uint32 TextSpeed){
         this->TextSpeed = TextSpeed;
@@ -91,15 +111,22 @@ namespace Potato{
     void Engine::ClearScene(){
         this->Scene.clear();
     }
-    void Engine::SceneAdd(Character* CharAddr){
+    void Engine::SceneAddCharacter(Character* CharAddr){
         this->Scene.push_back(CharAddr);
     }
-    void Engine::SceneRemove(Character* CharAddr){
+    void Engine::SceneRemoveCharacter(Character* CharAddr){
         for (int i=0;i<this->Scene.size();i++){
             if (this->Scene[i]==CharAddr){
                 this->Scene.erase(this->Scene.begin()+i);
                 return;
             }
         }
+    }
+    void Engine::SceneSetBackground(std::string BgSrc){
+        SDL_Texture* BgTexture = IMG_LoadTexture(this->Renderer, BgSrc.c_str());
+        if (BgTexture==nullptr)
+            return this->Error("Failed to load background image");
+        this->Background = BgSrc;
+        SDL_DestroyTexture(BgTexture);
     }
 }
